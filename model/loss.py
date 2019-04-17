8c6446c825458d7c438930b9c563c45770f3f31d
Min-Sheng <mason840929@gmail.com>
2019-04-17 08:41:29 +0800

    revise the bce loss

--- model/loss.py
+++ model/loss.py
@@ -8,4 +8,4 @@ def mse_loss(output, target):
     return F.mse_loss(output, target)
 
 def binary_cross_entropy(output, target):
-    return F.mse_loss(output, target)
+    return F.binary_cross_entropy(output, target)
